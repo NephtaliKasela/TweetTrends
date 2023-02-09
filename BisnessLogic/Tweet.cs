a33e5b888a5c3cf4dd966d001489b86e91540b19
Nephtali Kasela <nephtalikasela04@gmail.com>
2023-02-09 17:19:41 +0300

    Add project files.

---
+++ BisnessLogic/Tweet.cs
@@ -0,0 +1,12 @@
+﻿namespace BisnessLogic
+{
+    public class Tweet
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public List<string> Message { get; set; }
+        public double Sentiment { get; set; }
+    }
+}
---
+++ Eva_Test/Program.cs
@@ -0,0 +1,19 @@
+﻿using BisnessLogic;
+using DataAccess;
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Tweet_Repository Tweets = new Tweet_Repository();
+            List<Tweet> twt = Tweets.ReadTweets("family_tweets2014.txt");
+            Console.WriteLine($"{twt.Count}");
+            Console.WriteLine($"{twt[0].Latitude}");
+            Console.WriteLine($"{twt[0].Longitude}");
+        }
+    }
+}
