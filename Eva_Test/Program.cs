5d732a6cfee8e78645843d8175ee72c4c32e5452
Nephtali Kasela <nephtalikasela04@gmail.com>
2023-02-12 03:08:18 +0300

    Add some contents.

--- Eva_Test/Program.cs
+++ Eva_Test/Program.cs
@@ -8,24 +8,24 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Hello World!");
-            //Tweet_Repository Tweets = new Tweet_Repository();
-            //List<Tweet> twt = Tweets.ReadTweets("family_tweets2014.txt");
-            //Console.WriteLine($"{twt.Count}");
-                                                        //Console.WriteLine($"{twt[0].Latitude}");
-                                                        //Console.WriteLine($"{twt[0].Longitude}");
+            Console.WriteLine("Hello World!");
+            Tweet_Repository Tweets = new Tweet_Repository();
+            List<Tweet> twt = Tweets.ReadTweets("family_tweets2014.txt");
+            Console.WriteLine($"{twt.Count}");
+            Console.WriteLine($"{twt[0].Latitude}");
+            Console.WriteLine($"{twt[0].Longitude}");
 
-            //Sentiment_Repository sentiments = new Sentiment_Repository();
-            //List<Sentiment> sentmt = sentiments.ReadSentiments("sentiments.csv");
-            //Console.WriteLine($"{sentmt.Count}");
+            Sentiment_Repository sentiments = new Sentiment_Repository();
+            List<Sentiment> sentmt = sentiments.ReadSentiments("sentiments.csv");
+            Console.WriteLine($"{sentmt.Count}");
 
-            //Check_Sentiment chkSentiment = new Check_Sentiment();
-            //chkSentiment.Check(twt, sentmt);
+            Check_Sentiment chkSentiment = new Check_Sentiment();
+            chkSentiment.Check(twt, sentmt);
 
-            //foreach (Tweet t in twt)
-            //{
-            //    Console.WriteLine($"{t.Sentiment}");
-            //}
+            foreach (Tweet t in twt)
+            {
+                Console.WriteLine($"{t.Sentiment}");
+            }
 
             USA_States_Coordinates_Repository coor = new USA_States_Coordinates_Repository();
             List<State> states = coor.Read_USAmap_Coordinates("states.json");
